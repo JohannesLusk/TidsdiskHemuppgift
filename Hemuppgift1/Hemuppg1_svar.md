4250cbbfbce045240e1431cb3fc149ccb0c0adc7
Johannes lusk <lusk@kth.se>
2023-03-27 11:47:35 +0200

    fixade trean

--- Hemuppgift1/Hemuppg1_svar.md
+++ Hemuppgift1/Hemuppg1_svar.md
@@ -5,4 +5,4 @@ svar: signalen förändras inte, eller det finns ingen märkbar skillnad
 på default signalen och den modifierarde signalen.
 
 ## uppgift 2
-Nu epmasieras högre frekvenser
+signalen låter som om de lägre frekvenser har klippts bort
---
+++ Hemuppgift1/uppg2.m
@@ -0,0 +1,19 @@
+
+% Task 1 - MA filtering
+
+load handel; %gong, chirp, train, splat, laughter
+x = y;
+%soundsc(x,Fs);
+
+b0 = 0.5;
+b1 = -0.5;
+
+N = length(x); % Length of input signal
+y = zeros(size(x)); % Allocate space for outut
+
+y(1) = b0*x(1); % First sample, assuming x(0) = 0
+for n=2:N % Remaining samples
+    y(n) = b0*x(n) + b1*x(n-1);
+end
+
+soundsc(y,Fs);
